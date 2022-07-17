28b1656fd4ad018f82a1dba4bfd631391051b17a
rashevskyv <rashevsky.v@gmail.com>
2022-07-17 13:43:44 +0300

    trying to move from magnificent-popup module

--- stand_for_ukraine.js
+++ stand_for_ukraine.js
@@ -1,24 +1,28 @@
 $(function () {
+  $(".sfu-inline-holder").height(
+    ($(window).height() - $("#sfu-modal-popup").height()) / 2 - 60
+  );
+  $(window).resize(function () {
+    $(".sfu-inline-holder").height(
+      ($(window).height() - $("#sfu-modal-popup").height()) / 2 - 60
+    );
+  });
+});
+
+$(function () {
   let scenario2_sublink = "support-russia";
   let scenario2_link = "https://customfw.xyz/" + scenario2_sublink;
   let scenario1_cookie = "support-ukraine";
   let scenario2_cookie = "support-russia";
+  let days_of_cookie_live = 4;
 
-  if (getCookie(scenario1_cookie) || getCookie(scenario1_cookie)) {
-    document.getElementById("preloader").style.display = "none";
-  } else {
-    // $(document).ready(function () {
-      $(window).on("load", function () {
-        $(".preloader").fadeOut();
-        document.getElementById("preloader").style.display = "none";
-      // });
-    });
-  }
+  let date = new Date(Date.now() + 86400e3 * days_of_cookie_live).toUTCString();
 
-  if (document.getElementById("preloader").style.display == 'block') {
-    setTimeout(function(){
-      document.getElementById("preloader").style.display = "none";
-  }, 2000);
+  function closePopup() {
+    document.getElementById("sfu-modal-popup").style.display = "none";
+    document.getElementById("sfu-wrap").style.display = "none";
+    document.getElementById("sfu-bg").style.display = "none";
+    $("body").removeClass("hide-scroll");
   }
 
   function getCookie(name) {
@@ -32,6 +36,29 @@ $(function () {
     return matches ? decodeURIComponent(matches[1]) : undefined;
   }
 
+  if (getCookie(scenario1_cookie) || getCookie(scenario1_cookie)) {
+    // document.getElementById("preloader").style.display = "none";
+  } else {
+    // $(document).ready(function () {
+    $(window).on("load", function () {
+      $(".preloader").fadeOut();
+      if (document.getElementById("preloader")) {
+        document.getElementById("preloader").style.display = "none";
+      }
+      // });
+    });
+  }
+
+  if (document.getElementById("preloader")) {
+    if (document.getElementById("preloader").style.display == "block") {
+      setTimeout(function () {
+        if (document.getElementById("preloader")) {
+          document.getElementById("preloader").style.display = "none";
+        }
+      }, 2000);
+    }
+  }
+
   if (getCookie(scenario1_cookie)) {
     if (document.getElementById("popup-caller")) {
       document.getElementById("frame1").style.display = "none";
@@ -42,23 +69,14 @@ $(function () {
     getCookie(scenario2_cookie)
   ) {
     window.location.replace(scenario2_link);
-    $.magnificPopup.close();
+    closePopup();
   }
   if (window.location.href.includes(scenario2_sublink)) {
-    $.magnificPopup.close();
+    closePopup();
   }
-
-  $(".popup-modal").magnificPopup({
-    type: "inline",
-    preloader: false,
-    focus: "#username",
-    modal: true,
-  });
 
   $(document).on("click", ".popup-modal-support-ukraine", function (e) {
     e.preventDefault();
-    let date = new Date(Date.now() + 86400e3 * 7);
-    date = date.toUTCString();
     document.cookie = "support-ukraine=true; expires=" + date;
 
     document.getElementById("frame1").style.display = "none";
@@ -67,32 +85,21 @@ $(function () {
 
   $(document).on("click", ".popup-modal-support-russia", function (e) {
     e.preventDefault();
-    let date = new Date(Date.now() + 86400e3 * 7);
-    date = date.toUTCString();
     document.cookie = "support-russia=true; expires=" + date;
     window.location.replace(scenario2_link);
-    document.getElementById("mfp-modal-popup").style.display = "none";
   });
 
   $(document).on("click", ".popup-modal-close", function (e) {
     e.preventDefault();
-    $.magnificPopup.close();
-    document.getElementById("mfp-modal-popup").style.display = "none";
+    closePopup();
   });
 
   function getNumberOfDays() {
     const date1 = new Date("2/24/2022");
     const date2 = new Date();
-
-    // One day in milliseconds
     const oneDay = 1000 * 60 * 60 * 24;
-
-    // Calculating the time difference between two dates
     const diffInTime = date2.getTime() - date1.getTime();
-
-    // Calculating the no. of days between two dates
     const diffInDays = Math.round(diffInTime / oneDay);
-
     return diffInDays;
   }
 
@@ -100,11 +107,11 @@ $(function () {
 
   $(document).ready(function () {
     if (
-      !getCookie(scenario1_cookie) &&
-      !window.location.href.includes(scenario2_sublink)
+      getCookie(scenario1_cookie) ||
+      window.location.href.includes(scenario2_sublink)
     ) {
-      document.getElementById("mfp-modal-popup").style.display = "none";
-      $(".popup-modal").click();
+      //
+      closePopup();
     }
   });
 });
